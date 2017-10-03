cf462c854fbadbf4d6961fab76c34e3f5b2fc2a7
gfurmann <32485402+gfurmann@users.noreply.github.com>
2017-10-03 15:59:09 -0300

    Add files via upload

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,19 @@
+// Classe para listar negociações
+
 class ListaNegociacoes {
+
+    // O constructor têm um array de negociações
     constructor(){
         this._negociacoes = [];
     }
 
+    // O método adiciona recebe a negociação enviada pelo controller e manda para dentro do array
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
     }
 
+    // método para retornar as negociações mas sem permitir que estas sejam alteradas. Por tal motivo, retornamos um array
+    // vazio e concatenamos as nossas negociações nele, assim passamos uma "cópia" das negociações.
     get negociacoes(){
         return [].concat(this._negociacoes);
     }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,9 +1,13 @@
+//Classe para retornar a view
+
 class NegociacoesView {
 
+    // O constructor recebe um elemento do DOM que usaremos para colocar o modelo dentro
     constructor(elemento){
         this._elemento = elemento;
     }
 
+    // O template pega o array e por meio de uma template string retorna o html da página
     _template(model){
         return `
             <table class="table table-hover table-bordered">
@@ -18,16 +22,24 @@ class NegociacoesView {
             
             <tbody>
                 ${model.negociacoes.map(n => `
-                        <tr>
-                            <td>${DateHelper.DataParaTexto(n.data)}</td>
-                            <td>${n.quantidade}</td>
-                            <td>${n.valor}</td>
-                            <td>${n.volume}</td>
-                        </tr>
+                    <tr>
+                        <td>${DateHelper.DataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
                 `).join('')}
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>${
+                    (function(){
+                        let total = 0;
+                        model.negociacoes.forEach(n => total += n.volume);
+                        return total;
+                    })()
+                }</td>
             </tfoot>
         </table>
         `;
